3da69b2ef175029e5c33aace0b7710acac275673
Aishwarya Heshannavar <aishwaryaheshannavar@gmail.com>
2024-02-14 15:15:26 +0530

    Its working

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    //java: name has private access in com.driver.RWOnly
+    public static void main(String arg[]){
+        RWOnly obj=new RWOnly();
+        obj.setName("Priya");
+        System.out.println(obj.getName());
+    }
   
 }
